149c0832aee5c1cbbf2b485f31c7c1532245eccb
maaz-revcloud <maaz@revcloud.com>
2024-03-29 16:45:20 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,6 +5,9 @@ git pull origin main
 
 sudo su - <<EOF
 cd /home/ubuntu/ec2-deploy-test
-/root/.nvm/versions/node/v20.11.0/bin/npm install   
-/root/.nvm/versions/node/v20.11.0/bin/pm2 restart index.js 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+npm install   
+pm2 restart index.js 
 EOF
