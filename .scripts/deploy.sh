39146e34730f07e7d73cf804deb5b7f94739a106
maaz-revcloud <maaz@revcloud.com>
2024-03-29 16:28:12 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export PATH="$PATH:/usr/local/bin"  # Add /usr/local/bin to PATH
+
 cd /home/ubuntu/ec2-deploy-test
 git pull origin main
 
 sudo su <<EOF
 cd /home/ubuntu/ec2-deploy-test
-sudo npm install
-sudo pm2 restart index.js
+npm install
+pm2 restart index.js
 EOF
