86a6643c4622143057830e393e9a682ed13d4eeb
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:21:28 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,6 +8,8 @@ cd /home/ubuntu/ec2-deploy-test
 
 # Pull the latest changes from the main branch
 git pull origin main
+which npm
+which pm2
 
 # Define full paths to npm and pm2
 NPM_PATH=/usr/bin/npm  # Replace this with the actual path to npm if different
