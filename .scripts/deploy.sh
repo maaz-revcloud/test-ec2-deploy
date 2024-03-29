53d9c876c7732e71af66612cd59ef912c3c37104
maaz-revcloud <maaz@revcloud.com>
2024-03-29 16:31:04 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -5,6 +5,6 @@ git pull origin main
 
 sudo su <<EOF
 cd /home/ubuntu/ec2-deploy-test
-/root/.nvm/versions/node/v20.11.0/bin/npm/npm install   
+/root/.nvm/versions/node/v20.11.0/bin/npm install   
 /root/.nvm/versions/node/v20.11.0/bin/pm2 restart index.js 
 EOF
