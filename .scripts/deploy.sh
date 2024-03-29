48a36fec794aa6f6f47b5552c91fc64c2b9a24b8
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:29:08 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -10,11 +10,18 @@ cd /home/ubuntu/ec2-deploy-test
 git pull origin main
 ls
 
+# NODE_PATH=/root/.nvm/versions/node/v20.11.0/bin/node  install
+
 # # Define full paths to npm and pm2
 NPM_PATH=/root/.nvm/versions/node/v20.11.0/bin/npm # Replace this with the actual path to npm if different
 # PM2_PATH=/usr/bin/pm2  # Replace this with the actual path to pm2 if different
 
-$NODE_PATH $NPM_PATH install
+if [ ! -x "$NPM_PATH" ]; then
+    echo "Granting execute permissions to npm..."
+    chmod +x "$NPM_PATH"
+fi
+
+$NPM_PATH install
 
 # # Install dependencies using npm
 # $NPM_PATH install
