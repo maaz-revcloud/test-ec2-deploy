c7b3ceffcbf2e3ddc7fc8116120c154f18bfdb87
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:00:05 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -11,17 +11,15 @@ git pull origin main || { echo "Failed to pull latest changes from git repositor
 
 # Switch to root user and execute commands
 sudo su - <<EOF
-# Ensure Node.js and npm environment variables are set
-export NVM_DIR="/root/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Source .bashrc to ensure environment variables are set
+source /root/.bashrc
 
 # Navigate to the project directory
 cd /home/ubuntu/ec2-deploy-test || exit
 
 # Install dependencies using npm
-/root/.nvm/versions/node/$(node -v)/bin/npm install || { echo "Failed to install npm dependencies"; exit 1; }
+npm install || { echo "Failed to install npm dependencies"; exit 1; }
 
 # Restart the application using pm2
-/root/.nvm/versions/node/$(node -v)/bin/pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
+pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
 EOF
