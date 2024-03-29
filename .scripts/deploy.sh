0a4a7e974333fc4cb0fabbfe4f3698651da4af07
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:39:10 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Source the user's environment to ensure PATH settings are loaded
 source ~/.bashrc
-
-# Change directory to the project directory
 cd /home/ubuntu/ec2-deploy-test
 
-# Pull the latest changes from the main branch
 git pull origin main
 ls
 sudo su -
 
 
 NODE_PATH=/root/.nvm/versions/node/v20.11.0/bin/node
-
-# # Define full paths to npm and pm2
-NPM_PATH=/root/.nvm/versions/node/v20.11.0/bin/npm # Replace this with the actual path to npm if different
-# PM2_PATH=/usr/bin/pm2  # Replace this with the actual path to pm2 if different
+NPM_PATH=/root/.nvm/versions/node/v20.11.0/bin/npm 
+PM2_PATH=/root/.nvm/versions/node/v20.11.0/bin/pm2
 
 if [ ! -x "$NPM_PATH" ]; then
     echo "Granting execute permissions to npm..."
@@ -27,9 +21,4 @@ fi
 cd /home/ubuntu/ec2-deploy-test
 
 $NODE_PATH $NPM_PATH install
-
-# # Install dependencies using npm
-# $NPM_PATH install
-
-# # Restart the application using pm2
-# $PM2_PATH restart index.js
+$PM2_PATH restart index.js
