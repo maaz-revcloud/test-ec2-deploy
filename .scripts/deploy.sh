580c1897d978bc7fd4ffe632cfcd9b86038b60d5
maaz-revcloud <maaz@revcloud.com>
2024-03-29 16:56:20 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-cd /home/ubuntu/ec2-deploy-test
-git pull origin main
+# Navigate to the directory where the script resides
+cd "$(dirname "$0")" || exit
 
+# Navigate to the project directory
+cd /home/ubuntu/ec2-deploy-test || exit
+
+# Pull the latest changes from the main branch of the git repository
+git pull origin main || { echo "Failed to pull latest changes from git repository"; exit 1; }
+
+# Switch to root user and execute commands
 sudo su - <<EOF
-cd /home/ubuntu/ec2-deploy-test
-ls
-export PATH="/usr/local/bin:$PATH"
-npm install   
-pm2 restart index.js 
+# Navigate to the project directory
+cd /home/ubuntu/ec2-deploy-test || exit
+
+# Install dependencies using npm
+npm install || { echo "Failed to install npm dependencies"; exit 1; }
+
+# Restart the application using pm2
+pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
 EOF
