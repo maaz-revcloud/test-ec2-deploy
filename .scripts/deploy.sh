86d923da161d199e8a4a62a5696f1b008820ca2f
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:02:28 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -14,12 +14,17 @@ sudo su - <<EOF
 # Source .bashrc to ensure environment variables are set
 source /root/.bashrc
 
+# Ensure Node.js environment variables are set
+export NVM_DIR="/root/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # Navigate to the project directory
 cd /home/ubuntu/ec2-deploy-test || exit
 
-# Install dependencies using npm with absolute path
+# Install dependencies using npm
 /root/.nvm/versions/node/$(node -v)/bin/npm install || { echo "Failed to install npm dependencies"; exit 1; }
 
-# Restart the application using pm2 with absolute path
+# Restart the application using pm2
 /root/.nvm/versions/node/$(node -v)/bin/pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
 EOF
