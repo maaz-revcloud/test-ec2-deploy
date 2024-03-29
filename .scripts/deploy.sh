21700a8ea371c2d7bdea7abe1f8ef3aad4361dee
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:23:04 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -10,13 +10,14 @@ cd /home/ubuntu/ec2-deploy-test
 git pull origin main
 which npm
 which pm2
+ls
 
-# Define full paths to npm and pm2
+# # Define full paths to npm and pm2
 NPM_PATH=/usr/bin/npm  # Replace this with the actual path to npm if different
-PM2_PATH=/usr/bin/pm2  # Replace this with the actual path to pm2 if different
+# PM2_PATH=/usr/bin/pm2  # Replace this with the actual path to pm2 if different
 
-# Install dependencies using npm
-$NPM_PATH install
+# # Install dependencies using npm
+# $NPM_PATH install
 
-# Restart the application using pm2
-$PM2_PATH restart index.js
+# # Restart the application using pm2
+# $PM2_PATH restart index.js
