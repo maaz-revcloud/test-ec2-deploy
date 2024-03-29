fb4d54c17f05caf9658e9f14fa06a2f838330220
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:09:59 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,11 +2,11 @@
 
 cd /home/ubuntu/ec2-deploy-test
 git pull origin main
-sudo su - && npm install  
+sudo su - && npm install  && pm2 restart index.js 
 
-sudo su - <<EOF
-cd /home/ubuntu/ec2-deploy-test
-ls
-npm install   
-pm2 restart index.js 
-EOF
+# sudo su - <<EOF
+# cd /home/ubuntu/ec2-deploy-test
+# ls
+# npm install   
+# pm2 restart index.js 
+# EOF
