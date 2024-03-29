c087104dd54667867ee21795bd5f5af3b473b07a
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:25:37 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,13 +8,13 @@ cd /home/ubuntu/ec2-deploy-test
 
 # Pull the latest changes from the main branch
 git pull origin main
-which npm
-which pm2
 ls
 
 # # Define full paths to npm and pm2
-NPM_PATH=/usr/bin/npm  # Replace this with the actual path to npm if different
+NPM_PATH=/root/.nvm/versions/node/v20.11.0/bin/npm # Replace this with the actual path to npm if different
 # PM2_PATH=/usr/bin/pm2  # Replace this with the actual path to pm2 if different
+
+$NODE_PATH $NPM_PATH install
 
 # # Install dependencies using npm
 # $NPM_PATH install
