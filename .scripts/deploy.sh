45573ad4d3185184d067bf45a729e96e87951e35
maaz-revcloud <maaz@revcloud.com>
2024-03-29 17:04:22 +0500

    test commit

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -9,6 +9,10 @@ cd /home/ubuntu/ec2-deploy-test || exit
 # Pull the latest changes from the main branch of the git repository
 git pull origin main || { echo "Failed to pull latest changes from git repository"; exit 1; }
 
+# Determine the installed version of Node.js
+NODE_VERSION=$(node -v)
+echo "Node.js version: $NODE_VERSION"
+
 # Switch to root user and execute commands
 sudo su - <<EOF
 # Source .bashrc to ensure environment variables are set
@@ -18,8 +22,8 @@ source /root/.bashrc
 cd /home/ubuntu/ec2-deploy-test || exit
 
 # Install dependencies using npm
-/root/.nvm/versions/node/v<VERSION>/bin/npm install || { echo "Failed to install npm dependencies"; exit 1; }
+/root/.nvm/versions/node/$NODE_VERSION/bin/npm install || { echo "Failed to install npm dependencies"; exit 1; }
 
 # Restart the application using pm2
-/root/.nvm/versions/node/v<VERSION>/bin/pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
+/root/.nvm/versions/node/$NODE_VERSION/bin/pm2 restart index.js || { echo "Failed to restart the application using pm2"; exit 1; }
 EOF
